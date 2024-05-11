71688881f72cb81c2fb36f08675153be2ee40d85
Pavel Marek <pavel.marek@enso.org>
2024-05-11 10:32:59 +0200

    Rename alternative-lib to utils

--- alternative-lib/src/main/java/org/enso/lib/AlternativeLib.java
+++
@@ -1,7 +0,0 @@
-package org.enso.lib;
-
-public class AlternativeLib {
-    public static int add(int x, int y) {
-        return x - y;
-    }
-}
---
+++ utils/src/main/java/org/enso/lib/Utils.java
@@ -0,0 +1,5 @@
+package org.enso.lib;
+
+public class Utils {
+    // Access some package-protected stuff from org.enso.lib
+}
