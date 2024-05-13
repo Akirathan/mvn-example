a2e9040f6fedf7f92fce9d3708f7be4e369fc15f
Pavel Marek <pavel.marek@enso.org>
2024-05-13 18:25:39 +0200

    Move everything from utils to lib

--- lib/src/main/java/org/enso/lib/Utils.java
+++ lib/src/main/java/org/enso/lib/Utils.java
@@ -1,6 +1,5 @@
 package org.enso.lib;
 
-
 public class Utils {
     public static int callPackagePrivateMethodFromLib(int x, int y) {
         return Lib.packagePrivateCombine(x, y);
--- utils/src/main/java/module-info.java
+++
@@ -1,3 +0,0 @@
-module utils {
-  requires lib;
-}
