0cf768285540ee6f744238ad39c5c16d72e5c3b7
Pavel Marek <pavel.marek@enso.org>
2024-05-13 17:54:06 +0200

    Runner calls method from utils

--- runner/src/main/java/org/enso/runner/Runner.java
+++ runner/src/main/java/org/enso/runner/Runner.java
@@ -3,15 +3,21 @@ package org.enso.runner;
 import java.util.ServiceLoader;
 import org.enso.lib.Lib;
 import org.enso.lib.Service;
+import org.enso.lib.Utils;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Running lib with different service providers...");
+        int x = 1;
+        int y = 2;
+        int resFromUtils = Utils.callPackagePrivateMethodFromLib(x, y);
+        System.out.printf("Utils.callPackagePrivateMethodFromLib(%d, %d) returned %d %n", x, y, resFromUtils);
+        System.out.println("Running lib with different service providers:");
         ServiceLoader<Service> serviceLoader = ServiceLoader.load(Service.class);
         serviceLoader.forEach(serviceProvider -> {
             var lib = new Lib(serviceProvider);
-            var res = lib.combine(1, 2);
-            System.out.println("Lib with provider '" + serviceProvider.getClass().getName() + "' returned " + res);
+            var res = lib.combine(x, y);
+            System.out.printf("  lib.combine(provider=%s, x=%d, y=%d) returned %d %n",
+                serviceProvider.getClass().getName(), x, y, res);
         });
     }
 }
